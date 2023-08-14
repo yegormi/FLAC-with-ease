e5f635a14e7ed7f8620227817dc055eaa3f4868a
yegormi <egormiropoltsev79@gmail.com>
2023-08-14 22:25:43 +0300

    spaces fix

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ def perform_download(track_id: int, folder_path: str, filename: str) -> None:
     downloader = Downloader(track_id, folder_path, filename)
     downloader.with_progress_bar()
 
-
-
 def song_handling() -> Action:
     while True:
         print("Does this song match your request?")
@@ -40,7 +38,7 @@ def song_handling() -> Action:
         else:
             print("Invalid input. Please enter '1' or '2' or '3'.")
 
-def process_and_handle_songs(source_file_path: str, flac_folder_path: str) -> None:
+def process_songs(source_file_path: str, flac_folder_path: str) -> None:
     song = SongHandler(source_file_path)
     artist_local, title_local = song.extract()
     songs = song.request()
@@ -97,10 +95,9 @@ def main():
         if file.endswith("." + const.SOURCE_EXTENSION):
             mp3_filepath = os.path.join(const.SOURCE_FOLDER, file)
             try:
-                process_and_handle_songs(mp3_filepath, const.FLAC_FOLDER)
+                process_songs(mp3_filepath, const.FLAC_FOLDER)
             except Exception as e:
-                error_message = f"An error occurred while processing {mp3_filepath}: {str(e)}"
-                print(error_message)
+                print(f"An error occurred while processing {mp3_filepath}: {str(e)}")
              
 if __name__ == "__main__":
     main()
