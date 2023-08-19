ca2575a79afce6c920b1ceef3061185c26b35c23
Yegor Myropoltsev <egormiropoltsev79@gmail.com>
2023-08-19 18:53:09 +0300

    delete deprecated flac folder constant

--- config.py
+++ config.py
@@ -2,4 +2,3 @@ SOURCE_EXTENSION    = "mp3"
 DEBUG               = True
 DEBUG_COMPLEX       = False
 SOURCE_FOLDER       = "/Users/yegormyropoltsev/Desktop/mp3"
-# FLAC_FOLDER         = "path/to/flac/folder"
